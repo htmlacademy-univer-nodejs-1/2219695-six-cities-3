e7d94b5526350ae7fa3021bff38e3cf641b3d7fd
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-02-25 20:41:52 +0500

    feat: add cli-application

---
+++ src/cli/cli-application.ts
@@ -0,0 +1,17 @@
+import {Command} from './commands/command.interface';
+
+type CommandCollection = Record<string, Command>;
+
+export class CLIApplication {
+  private commands: CommandCollection = {};
+
+  public registerCommands(commandList: Command[]): void {
+    commandList.forEach((command) => {
+      if (Object.hasOwn(this.commands, command.getName())) {
+        throw new Error(`Command ${command.getName()} is already registered`);
+      }
+
+      this.commands[command.getName()] = command;
+    });
+  }
+}
