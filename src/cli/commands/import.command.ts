f20723d9b37134f3c9c38825f318c58497fcc0a4
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-08 19:55:04 +0500

    refactor: change import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,26 +1,38 @@
 import {Command} from './command.interface.js';
 import {TSVFileReader} from '../../shared/libs/file-reader/index.js';
 import chalk from 'chalk';
+import {createOffer, getErrorMessage} from '../../shared/helpers/index.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
     return '--import';
   }
 
-  public execute(...parameters: string[]) {
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported`);
+  }
+
+  public async execute(...parameters: string[]) {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
-      fileReader.read();
-      console.log(fileReader.toArray());
+      await fileReader.read();
     } catch (error) {
       if (!(error instanceof Error)) {
         throw error;
       }
 
       console.error(chalk.red(`Can't import data from file: ${filename}`));
-      console.error(chalk.red(`Details: ${error.message}`));
+      console.error(chalk.red(getErrorMessage(error)));
     }
   }
 }
