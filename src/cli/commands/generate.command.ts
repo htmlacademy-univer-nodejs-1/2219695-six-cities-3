fee60ecfc58fa57e1711983172b93027122db2e8
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-08 19:19:20 +0500

    feat: add generator in generate command

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,6 +1,8 @@
-import {Command} from './command.interface';
+import {Command} from './command.interface.js';
 import {MockServerData} from '../../shared/types/index.js';
 import got from 'got';
+import {TSVOfferGenerator} from '../../shared/libs/offer-generator/index.js';
+import {appendFile} from 'node:fs/promises';
 
 
 export class GenerateCommand implements Command {
@@ -14,6 +16,17 @@ export class GenerateCommand implements Command {
     }
   }
 
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        'utf-8'
+      );
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
@@ -24,6 +37,8 @@ export class GenerateCommand implements Command {
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created`);
     } catch (error: unknown) {
       console.error('Can\'t generate data');
 
