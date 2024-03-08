cb767b943a671d73e2b40d206b6838519615b6cc
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-08 19:13:15 +0500

    feat: add get mock data request

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,12 +1,35 @@
 import {Command} from './command.interface';
+import {MockServerData} from '../../shared/types/index.js';
+import got from 'got';
+
 
 export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters) {
+  public async execute(...parameters) {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
