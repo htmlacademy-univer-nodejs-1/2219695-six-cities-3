48f752ce0b15cb52dfa14c214b9d12ec2302c327
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-29 13:57:19 +0500

    feat: extends user-document

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,13 +1,16 @@
 import {Document, model, Schema} from 'mongoose';
 import {User} from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
   name: String,
   email: String,
   avatar: String,
   password: String,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
