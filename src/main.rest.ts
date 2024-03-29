920b914ca604d5cb764c1906b220cb14ce70ec35
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-29 16:55:40 +0500

    feat: add DI in default-user-service

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,9 +3,13 @@ import {RestApplication} from './rest/index.js';
 import {Container} from 'inversify';
 import {Component} from './shared/types/index.js';
 import {createRestApplicationContainer} from './rest/rest.container.js';
+import {createUserContainer} from './shared/modules/user/user.container.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+  );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
