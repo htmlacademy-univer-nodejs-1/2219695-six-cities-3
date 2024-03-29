ee3dd8a07899d0fb7a3ffa07cf3bfab2a7ea23ff
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-03-29 15:43:22 +0500

    feat: add define user model and schema

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,39 @@
 import {User, UserType} from '../../types/index.js';
+import {getModelForClass, prop} from '@typegoose/typegoose';
 
 export class UserEntity implements User {
+  @prop({
+    required: true,
+    type: () => String
+  })
   public name: string;
+
+  @prop({
+    unique: true,
+    required: true,
+    type: () => String
+  })
   public email: string;
+
+  @prop({
+    required: false,
+    default: '',
+    type: () => String
+  })
   public avatar: string;
+
+  @prop({
+    required: true,
+    type: () => String
+  })
   public password: string;
+
+  @prop({
+    required: true,
+    type: () => String,
+    enum: UserType
+  })
   public type: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
