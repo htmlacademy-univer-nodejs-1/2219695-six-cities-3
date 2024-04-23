b9290b087db74adbaec365c5cb2a253e87a1a9ff
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-23 21:21:15 +0500

    feat: add user-controller

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -6,6 +6,7 @@ import {DatabaseClient} from '../shared/libs/database-client/index.js';
 import {getMongoURI} from '../shared/helpers/index.js';
 import express, {Express} from 'express';
 import {Controller, ExceptionFilter} from '../shared/libs/rest/index.js';
+import {UserController} from '../shared/modules/user/index.js';
 
 @injectable()
 export class RestApplication {
@@ -17,6 +18,7 @@ export class RestApplication {
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(Component.UserController) private readonly userController: UserController
   ) {
     this.server = express();
   }
@@ -40,6 +42,7 @@ export class RestApplication {
 
   private async _initControllers() {
     this.server.use('/offers', this.offerController.router);
+    this.server.use('/users', this.userController.router);
   }
 
   public async _initMiddleware() {
