1f5afb342bd3a630b8bd6ba7ae492bac9b0b1c91
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-23 21:01:08 +0500

    feat: use app-exception-filter

--- src/rest/rest.container.ts
+++ src/rest/rest.container.ts
@@ -4,6 +4,7 @@ import {DatabaseClient, MongoDatabaseClient} from '../shared/libs/database-clien
 import {Logger, PinoLogger} from '../shared/libs/logger/index.js';
 import {Component} from '../shared/types/index.js';
 import {Config, RestConfig, RestSchema} from '../shared/libs/config/index.js';
+import {AppExceptionFilter, ExceptionFilter} from '../shared/libs/rest/index.js';
 
 export function createRestApplicationContainer() {
   const restApplicationContainer = new Container();
@@ -12,6 +13,7 @@ export function createRestApplicationContainer() {
   restApplicationContainer.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
   restApplicationContainer.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
   restApplicationContainer.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  restApplicationContainer.bind<ExceptionFilter>(Component.ExceptionFilter).to(AppExceptionFilter).inSingletonScope();
 
   return restApplicationContainer;
 }
