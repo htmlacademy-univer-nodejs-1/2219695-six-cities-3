29fd99a8e82844877bc6fc2343690a2203c46791
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-23 21:51:30 +0500

    feat: add handle http-error in app-exception-filter

--- src/shared/libs/rest/exception-filter/app-exception-filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception-filter.ts
@@ -5,6 +5,8 @@ import {Logger} from '../../logger/index.js';
 import {Error} from 'mongoose';
 import {NextFunction, Request, Response} from 'express';
 import {StatusCodes} from 'http-status-codes';
+import {HttpError} from '../errors/index.js';
+import {createErrorObject} from '../../../helpers/index.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -12,10 +14,25 @@ export class AppExceptionFilter implements ExceptionFilter {
     this.logger.info('Register AppExceptionFilter');
   }
 
-  public catch(error: Error, _req: Request, res: Response, _next: NextFunction): void {
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} — ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
     this.logger.error(error.message, error);
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({error: error.message});
+      .json(createErrorObject(error.message));
+  }
+
+  public catch(error: Error | HttpError, req: Request, res: Response, next: NextFunction): void {
+    if (error instanceof HttpError) {
+      return this.handleHttpError(error, req, res, next);
+    }
+
+    this.handleOtherError(error, req, res, next);
   }
 }
