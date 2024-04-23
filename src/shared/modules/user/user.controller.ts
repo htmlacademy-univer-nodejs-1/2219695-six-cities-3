856246a7c7d495974693650232034f877e28d1c0
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-23 22:10:28 +0500

    feat: add login in user-controller

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -9,6 +9,7 @@ import {Config, RestSchema} from '../../libs/config/index.js';
 import {StatusCodes} from 'http-status-codes';
 import {fillDTO} from '../../helpers/index.js';
 import {UserRdo} from './rdo/user.rdo.js';
+import {LoginUserRequest} from './login-user-request.type.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -25,6 +26,12 @@ export class UserController extends BaseController {
       method: HttpMethod.Post,
       handler: this.create
     });
+
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Post,
+      handler: this.login
+    });
   }
 
   public async create({body}: CreateUserRequest, res: Response): Promise<void> {
@@ -40,5 +47,23 @@ export class UserController extends BaseController {
 
     const result = await this.userService.create(body, this.configService.get('SALT'));
     this.created(res, fillDTO(UserRdo, result));
+  }
+
+  public async login({body}: LoginUserRequest, _res: Response): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (!existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email ${body.email} not found.`,
+        'UserController',
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController',
+    );
   }
 }
