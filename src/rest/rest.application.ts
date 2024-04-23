937f1398a18cc33e005ddf6f309331d5301a3190
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-04-23 22:29:32 +0500

    chore: delete unused imports

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -6,8 +6,6 @@ import {DatabaseClient} from '../shared/libs/database-client/index.js';
 import {getMongoURI} from '../shared/helpers/index.js';
 import express, {Express} from 'express';
 import {Controller, ExceptionFilter} from '../shared/libs/rest/index.js';
-import {UserController} from '../shared/modules/user/index.js';
-import {OfferController} from '../shared/modules/offer/index.js';
 
 @injectable()
 export class RestApplication {
