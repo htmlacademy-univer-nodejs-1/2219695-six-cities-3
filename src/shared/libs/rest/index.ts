db6b93d2c78011b515f4c9f6323df7ac02daa534
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-05-09 23:56:20 +0500

    feat: add ValidateDtoMiddleware

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -9,3 +9,4 @@ export * from './types/request.params.type.js';
 export * from './errors/index.js';
 export * from './middleware/middleware.interface.js';
 export * from './middleware/validate-objectid.middleware.js';
+export * from './middleware/validate-dto.middleware.js';
