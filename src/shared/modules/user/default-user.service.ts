6e49d5b5ad2d7869b2e70ef816fd6a858e74c415
Dmitry <dmitriy_kozachenko_02@mail.ru>
2024-05-26 11:41:54 +0500

    fix: impl updateById method in userService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -6,6 +6,7 @@ import {inject, injectable} from 'inversify';
 import {Component} from '../../types/index.js';
 import {Logger} from '../../libs/logger/index.js';
 import {DEFAULT_AVATAR_FILE_NAME} from './user.constant.js';
+import {UpdateUserDto} from './dto/update-user.dto.js';
 
 @injectable()
 export class DefaultUserService implements UserService {
@@ -40,5 +41,9 @@ export class DefaultUserService implements UserService {
     }
 
     return this.create(dto, salt);
+  }
+
+  public async updateById(userId: string, dto: UpdateUserDto): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findByIdAndUpdate(userId, dto, { new: true });
   }
 }
